ee4a3b68d3ecba0ac96c07ab8be3213ec1e7acad
J. Muratet-Decker <contactpod@gmail.com>
2023-01-02 12:03:42 +0100

    Big Update 😎

---
+++ js/main.js
@@ -0,0 +1,40 @@
+history.scrollRestoration = 'manual';
+
+var lastPos = document.body.scrollTop || document.documentElement.scrollTop,
+    perspective = 500,
+    zSpacing = -2000;
+zVals = [],
+    $frames = $(".frame"), 
+    frames = $frames.toArray(), 
+    numFrames = $frames.length;
+for (var i = 0; i < numFrames; i++) { zVals.push((numFrames - i) * zSpacing); }
+
+$(window).scroll(function (d, e) {
+    var top = document.body.scrollTop || document.documentElement.scrollTop,
+        delta = lastPos - top;
+        lastPos = top;
+    for (var i = 0; i < numFrames; i++) {
+        var newZVal = (zVals[i] += (delta * -1.5)),
+            frame = frames[i], 
+            transform = "translateZ(" + newZVal + "px)", 
+            opacity = newZVal < 200 ? 1 : 1 - parseInt((newZVal - 200) / (perspective - 200) * 10) / 10, 
+            display = newZVal > perspective ? "none" : "block";
+            frame.setAttribute("style","-webkit-transform:" + transform + ";-moz-transform:" + transform + ";display:" + display + ";opacity:" + opacity);
+    }
+});
+
+$( "#start" ).click(function() {
+    $("html, body").animate({
+        scrollTop: $('html, body').get(0).scrollHeight
+    }, 25000, 'linear');
+    $("#stop").show();
+    $("#start").hide();
+});
+
+$( "#stop" ).click(function() {
+    var myDiv = $( "html, body" );
+    myDiv.clearQueue();
+    myDiv.stop();
+    $("#stop").hide();
+    $("#start").show();
+});
